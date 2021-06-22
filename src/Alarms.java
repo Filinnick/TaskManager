e3fb6dcd3a5be9e3ee3593c758070683e641e306
Filinnick <69903959+Filinnick@users.noreply.github.com>
2021-06-22 22:42:07 +0400

    Update Alarms.java

--- src/Alarms.java
+++ src/Alarms.java
@@ -10,7 +10,6 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 //TODO добавить вызов доп окна с текстом задачи и кнопками "Завершнить" и "Отложить (25мин)"
-//кнопка "Отложить (25мин)" откладывает задачу на одну помодорку (25мин) ScheduledExecutorService
 public class Alarms {
 
     ScheduledExecutorService exe = Executors.newScheduledThreadPool(1);
